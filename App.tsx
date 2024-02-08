96e110d41e5194885af511800bccb19d5fb7857a
jatinhemnani01 <jatinhemnani01@gmail.com>
2024-02-08 13:06:53 +0530

    pruchase working

--- App.tsx
+++ App.tsx
@@ -1,10 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import { Platform, StyleSheet, Text, View } from "react-native";
+import Purchases, { LOG_LEVEL } from "react-native-purchases";
 
 export default function App() {
+  useEffect(() => {
+    async function setupPurchases() {
+      Purchases.setLogLevel(LOG_LEVEL.DEBUG);
+      if (Platform.OS === "android") {
+        Purchases.configure({ apiKey: "goog_gGxArHKrmaNHKqOSVLLiPWnlwYL" });
+        console.log(await Purchases.getOfferings());
+        console.log(await Purchases.isConfigured());
+      }
+    }
+    setupPurchases();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Opasasas up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +27,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
