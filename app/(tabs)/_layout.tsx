38e0f9f7741a5b24c0396ae89348188b66562ad5
jatinhemnani01 <jatinhemnani01@gmail.com>
2024-02-10 23:16:14 +0530

    added profile and popular route

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,15 +1,39 @@
-import { Tabs } from 'expo-router/tabs';
+import { Tabs } from "expo-router/tabs";
 export default function AppLayout() {
   return (
     <Tabs>
       <Tabs.Screen
         name="index"
         options={{
-            title: 'Home',
-            tabBarLabelStyle: { fontSize: 15},
+          title: "Home",
+          headerTitleAlign:"center",
+          tabBarLabelStyle: { fontSize: 15 },
         }}
       />
-      <Tabs.Screen name='search'  />
+      <Tabs.Screen
+        name="search"
+        options={{
+          title: "Search",
+          headerTitleAlign:"center",
+          tabBarLabelStyle: { fontSize: 15 },
+        }}
+      />
+      <Tabs.Screen
+        name="popular"
+        options={{
+          title: "Popular",
+          headerTitleAlign:"center",
+          tabBarLabelStyle: { fontSize: 15 },
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          headerTitleAlign:"center",
+          tabBarLabelStyle: { fontSize: 15 },
+        }}
+      />
     </Tabs>
   );
 }
