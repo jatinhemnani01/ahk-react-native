41a9d6cba9b471474a962c4cbb8f745b83cb5929
jatinhemnani01 <jatinhemnani01@gmail.com>
2024-02-12 23:55:52 +0530

    video speed ui improve

--- src/components/VideoSpeedControl.tsx
+++ src/components/VideoSpeedControl.tsx
@@ -1,5 +1,8 @@
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text } from "react-native";
+import { Icon, Button, color } from "@rneui/base";
 import React from "react";
+import tw from "twrnc";
+import { colors } from "../constants/colors";
 
 interface VideoSpeedControlProps {
   speed: number;
@@ -13,30 +16,19 @@ export default function VideoSpeedControl({
   speed,
 }: VideoSpeedControlProps) {
   return (
-    <View>
-      <View style={styles.buttonsContainer}>
-        <Button title="+" onPress={handleIncrement} />
-        <Button title="-" onPress={handleDecrement} />
+    <View style={tw`flex justify-center items-center mt-10`}>
+      <Text style={tw`font-semibold mb-5 text-xl`}>
+        Change Scale/Tempo/Speed
+      </Text>
+      <View style={tw`flex flex-row gap-2 justify-center items-center`}>
+        <Button onPress={handleIncrement} radius={"xl"} color={colors.primary}>
+          <Icon name="plus" color={"white"} size={20} type="font-awesome" />
+        </Button>
+        <Text style={tw`font-bold text-xl`}>{speed.toFixed(2).toString()}</Text>
+        <Button onPress={handleDecrement} radius={"xl"} color={colors.primary}>
+          <Icon name="minus" color={"white"} size={20} type="font-awesome" />
+        </Button>
       </View>
-      <Text>Speed: {speed.toFixed(2).toString()}</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flexDirection: "row",
-    alignItems: "center",
-  },
-  buttonsContainer: {
-    flexDirection: "row",
-    marginRight: 10,
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: "gray",
-    borderRadius: 5,
-    paddingHorizontal: 10,
-    minWidth: 80,
-  },
-});
