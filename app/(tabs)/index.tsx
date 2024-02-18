5b96059aea41b30345ddc7b913e8c97bacf44c8d
jatinhemnani01 <jatinhemnani01@gmail.com>
2024-02-18 18:23:08 +0530

    status bar home

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,32 +2,36 @@ import { Pressable, View, Text, ScrollView } from "react-native";
 import KaraokeTile from "../../src/components/KaraokeTile";
 import tw from "twrnc";
 import { router } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 export default function AppLayout() {
   return (
-    <ScrollView>
-      <View>
-        <Pressable
-          onPress={() => router.navigate("/free")}
-          style={tw`flex flex-row justify-center m-2`}
-        >
-          <Text style={tw`text-lg text-blue-500 font-semibold`}>
-            Explore Free Karaoke
-          </Text>
-        </Pressable>
-      </View>
-      <KaraokeTile />
-      <KaraokeTile />
-      <KaraokeTile />
-      <KaraokeTile />
-      <KaraokeTile />
-      <KaraokeTile />
-      <KaraokeTile />
-      <KaraokeTile />
-      <KaraokeTile />
-      <KaraokeTile />
-      <KaraokeTile />
-      <KaraokeTile />
-      <KaraokeTile />
-    </ScrollView>
+    <>
+      <StatusBar style="light" />
+      <ScrollView>
+        <View>
+          <Pressable
+            onPress={() => router.navigate("/free")}
+            style={tw`flex flex-row justify-center m-2`}
+          >
+            <Text style={tw`text-lg text-blue-500 font-semibold`}>
+              Explore Free Karaoke
+            </Text>
+          </Pressable>
+        </View>
+        <KaraokeTile />
+        <KaraokeTile />
+        <KaraokeTile />
+        <KaraokeTile />
+        <KaraokeTile />
+        <KaraokeTile />
+        <KaraokeTile />
+        <KaraokeTile />
+        <KaraokeTile />
+        <KaraokeTile />
+        <KaraokeTile />
+        <KaraokeTile />
+        <KaraokeTile />
+      </ScrollView>
+    </>
   );
 }
