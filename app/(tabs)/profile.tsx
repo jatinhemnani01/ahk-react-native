b620c50e5eb84247a15dfdbb6e08b756eb9386ba
jatinhemnani01 <jatinhemnani01@gmail.com>
2024-02-18 14:35:18 +0530

    added pricing card

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,9 +1,11 @@
-import { Button } from "@rneui/base";
+import { Button, PricingCard } from "@rneui/base";
 import { StatusBar } from "expo-status-bar";
 import { useEffect } from "react";
-import { Platform, StyleSheet, Text, View } from "react-native";
+import { Platform, ScrollView, View } from "react-native";
 import Purchases, { LOG_LEVEL } from "react-native-purchases";
 import RevenueCatUI from "react-native-purchases-ui";
+import { colors } from "../../src/constants/colors";
+import tw from "twrnc";
 
 export default function profile() {
   function buy() {
@@ -15,20 +17,30 @@ export default function profile() {
       Purchases.setLogLevel(LOG_LEVEL.DEBUG);
       if (Platform.OS === "android") {
         Purchases.configure({ apiKey: "goog_gGxArHKrmaNHKqOSVLLiPWnlwYL" });
-        RevenueCatUI.presentPaywall();
       }
     }
     setupPurchases();
   }, []);
 
-  return <Button title={"Buy Now"} onPress={buy} />;
+  return (
+    <>
+      <View style={tw`flex justify-center flex-row flex-1 items-center`}>
+        <ScrollView>
+          <PricingCard
+            containerStyle={tw`rounded-xl`}
+            color={colors.primary}
+            title="Free"
+            price="$0"
+            info={["Free Karaoke", "Basic Support", "Change Speed/Scale"]}
+            button={{
+              title: "Buy Now",
+              onPress: buy,
+              buttonStyle: tw`rounded-lg`,
+            }}
+          />
+        </ScrollView>
+        <StatusBar style="light" />
+      </View>
+    </>
+  );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
