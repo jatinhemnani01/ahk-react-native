4f398d000589a9d676bdcc333725dfd55d43836c
jatinhemnani01 <jatinhemnani01@gmail.com>
2024-02-18 14:35:44 +0530

    status bar white

--- app/free.tsx
+++ app/free.tsx
@@ -1,10 +1,14 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { StatusBar } from 'expo-status-bar'
 
 export default function Free() {
   return (
+    <>
+    <StatusBar style="light" />
     <View>
       <Text>Free Karaoke</Text>
     </View>
+    </>
   )
 }
