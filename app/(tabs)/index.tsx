0e27ad6213c51cc8c6a0758910f6615ab9d00bd5
jatinhemnani01 <jatinhemnani01@gmail.com>
2024-02-20 15:46:21 +0530

    error handling

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,18 +1,11 @@
-import { Pressable, View, Text, ScrollView, Button } from "react-native";
-import KaraokeTile from "../../src/components/common/KaraokeTile";
+import { Pressable, View, Text } from "react-native";
 import tw from "twrnc";
 import { router } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import isProStore from "../../src/state/isPro";
-import useFetch from "../../src/hooks/useFetch";
-import { FlashList } from "@shopify/flash-list";
-import BASE_URL from "../../src/constants/base_url";
 import KaraokeList from "../../src/components/home/KaraokeList";
 export default function AppLayout() {
   const isPro = isProStore((state) => state.isPro);
-  const { data, error, isLoading, setData } = useFetch(
-    `${BASE_URL}/v2/all?page=1&limit=10`
-  );
 
   return (
     <>
@@ -31,21 +24,6 @@ export default function AppLayout() {
 
         <KaraokeList />
       </View>
-      {/* <ScrollView>
-        <View>
-          {!isPro && (
-            <Pressable
-              onPress={() => router.navigate("/free")}
-              style={tw`flex flex-row justify-center m-2`}
-            >
-              <Text style={tw`text-lg text-blue-500 font-semibold`}>
-                Explore Free Karaoke
-              </Text>
-            </Pressable>
-          )}
-        </View>
-          <KaraokeList />
-      </ScrollView> */}
     </>
   );
 }
--- src/components/home/KaraokeList.tsx
+++ src/components/home/KaraokeList.tsx
@@ -1,14 +1,18 @@
-import { View, Text, ScrollView, Dimensions, FlatList } from "react-native";
+import {
+  View,
+  Text, ActivityIndicator
+} from "react-native";
 import React from "react";
 import BASE_URL from "../../constants/base_url";
 import useFetch from "../../hooks/useFetch";
 import { FlashList } from "@shopify/flash-list";
 import KaraokeTile from "../common/KaraokeTile";
 import { KaraokeListItem } from "../../types/KaraokeListItemType";
+import tw from "twrnc";
 
 export default function KaraokeList() {
-  const { data, error, isLoading, setData } = useFetch(
-    `${BASE_URL}/v2/all?page=1&limit=10`
+  const { data, error, setData } = useFetch(
+    `${BASE_URL}/v2/alla?page=1&limit=10`
   );
 
   const RenderKaraokeList = ({ item }: { item: KaraokeListItem }) => {
@@ -21,17 +25,26 @@ export default function KaraokeList() {
     setData([...data, ...newDate]);
   }
 
+  if (error) {
+    return (
+      <View style={tw`flex justify-center flex-1 items-center`}>
+        <Text style={tw`font-bold text-lg`}>Something Went Wrong!</Text>
+      </View>
+    );
+  }
+
   return (
-      <FlashList
-        data={data}
-        estimatedItemSize={300}
-        renderItem={RenderKaraokeList}
-        ListFooterComponent={() => <Text>Loading More...</Text>}
-        onEndReached={() => {
-          fetchMore()
-          console.log("end");
-          
-        }}
-      />
+    <FlashList
+      data={data}
+      estimatedItemSize={1000}
+      renderItem={RenderKaraokeList}
+      ListFooterComponent={() => (
+        <ActivityIndicator size="large" color="#0000ff" />
+      )}
+      onEndReached={() => {
+        fetchMore();
+        console.log("end");
+      }}
+    />
   );
 }
