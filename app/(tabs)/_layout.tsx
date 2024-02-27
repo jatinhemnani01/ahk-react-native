b701f9f6d6bd20fe3fbb42d8c8bee2d5974b1bb4
jatinhemnani01 <jatinhemnani01@gmail.com>
2024-02-27 16:18:52 +0530

    added settings page

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -6,81 +6,117 @@ import { StatusBar } from "expo-status-bar";
 export default function TabLayout() {
   return (
     <>
-    <StatusBar style="light" />
-    <Tabs>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: "Home",
-          headerTitleAlign: "center",
-          tabBarLabelStyle: { fontSize: 15, color: "black" },
-          headerTitleStyle: { color: "white" },
-          headerStyle: { backgroundColor: colors.primary },
-          tabBarIcon: ({ focused }) => {
-            return (
-              <View>
-                <Icon name="home" color={focused ? colors.primary : "grey"} />
-              </View>
-            );
-          },
-        }}
-      />
-      <Tabs.Screen
-        name="search"
-        options={{
-          title: "Search",
-          headerTitleAlign: "center",
-          tabBarLabelStyle: { fontSize: 15, color: "black" },
-          headerTitleStyle: { color: "white" },
-          headerStyle: { backgroundColor: colors.primary },
-          tabBarIcon: ({ focused }) => {
-            return (
-              <View>
-                <Icon name="search" color={focused ? colors.primary : "grey"} />
-              </View>
-            );
-          },
-        }}
-      />
-      <Tabs.Screen
-        name="popular"
-        options={{
-          title: "Popular",
-          headerTitleAlign: "center",
-          tabBarLabelStyle: { fontSize: 15, color: "black" },
-          headerTitleStyle: { color: "white" },
-          headerStyle: { backgroundColor: colors.primary },
-          tabBarIcon: ({ focused }) => {
-            return (
-              <View>
-                <Icon
-                  name="fire"
-                  type="font-awesome-5"
-                  color={focused ? colors.primary : "grey"}
-                />
-              </View>
-            );
-          },
-        }}
-      />
-      <Tabs.Screen
-        name="profile"
-        options={{
-          title: "Profile",
-          headerTitleAlign: "center",
-          tabBarLabelStyle: { fontSize: 15, color: "black" },
-          headerTitleStyle: { color: "white" },
-          headerStyle: { backgroundColor: colors.primary },
-          tabBarIcon: ({ focused }) => {
-            return (
-              <View>
-                <Icon name="person" color={focused ? colors.primary : "grey"} />
-              </View>
-            );
-          },
-        }}
-      />
-    </Tabs>
+      <StatusBar style="light" />
+      <Tabs>
+        <Tabs.Screen
+          name="index"
+          options={{
+            headerRight: () => (
+              <Icon
+                name="menu"
+                type="ionicons"
+                color="white"
+                size={30}
+                style={{ marginRight: 20 }}
+              />
+            ),
+            title: "Home",
+            headerTitleAlign: "center",
+            tabBarLabelStyle: { fontSize: 15, color: "black" },
+            headerTitleStyle: { color: "white" },
+            headerStyle: { backgroundColor: colors.primary },
+            tabBarIcon: ({ focused }) => {
+              return (
+                <View>
+                  <Icon name="home" color={focused ? colors.primary : "grey"} />
+                </View>
+              );
+            },
+          }}
+        />
+        <Tabs.Screen
+          name="search"
+          options={{
+            title: "Search",
+            headerTitleAlign: "center",
+            tabBarLabelStyle: { fontSize: 15, color: "black" },
+            headerTitleStyle: { color: "white" },
+            headerStyle: { backgroundColor: colors.primary },
+            tabBarIcon: ({ focused }) => {
+              return (
+                <View>
+                  <Icon
+                    name="search"
+                    color={focused ? colors.primary : "grey"}
+                  />
+                </View>
+              );
+            },
+          }}
+        />
+        <Tabs.Screen
+          name="popular"
+          options={{
+            title: "Popular",
+            headerTitleAlign: "center",
+            tabBarLabelStyle: { fontSize: 15, color: "black" },
+            headerTitleStyle: { color: "white" },
+            headerStyle: { backgroundColor: colors.primary },
+            tabBarIcon: ({ focused }) => {
+              return (
+                <View>
+                  <Icon
+                    name="fire"
+                    type="font-awesome-5"
+                    color={focused ? colors.primary : "grey"}
+                  />
+                </View>
+              );
+            },
+          }}
+        />
+        <Tabs.Screen
+          name="profile"
+          options={{
+            title: "Profile",
+            headerTitleAlign: "center",
+            tabBarLabelStyle: { fontSize: 15, color: "black" },
+            headerTitleStyle: { color: "white" },
+            headerStyle: { backgroundColor: colors.primary },
+            tabBarIcon: ({ focused }) => {
+              return (
+                <View>
+                  <Icon
+                    name="person"
+                    color={focused ? colors.primary : "grey"}
+                  />
+                </View>
+              );
+            },
+          }}
+        />
+
+        <Tabs.Screen
+          name="settings"
+          options={{
+            title: "Settings",
+            headerTitleAlign: "center",
+            tabBarLabelStyle: { fontSize: 15, color: "black" },
+            headerTitleStyle: { color: "white" },
+            headerStyle: { backgroundColor: colors.primary },
+            tabBarIcon: ({ focused }) => {
+              return (
+                <View>
+                  <Icon
+                    name="settings"
+                    color={focused ? colors.primary : "grey"}
+                  />
+                </View>
+              );
+            },
+          }}
+        />
+      </Tabs>
     </>
   );
 }
