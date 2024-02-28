bcd0c96ef3be5c2bd763e704c46d7a11507d9170
jatinhemnani01 <jatinhemnani01@gmail.com>
2024-02-29 00:02:13 +0530

    storing liked songs on storage

--- app/(tabs)/favourite.tsx
+++ app/(tabs)/favourite.tsx
@@ -1,10 +1,20 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React, { useEffect } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function Favourite() {
+  async function getLikedSongs() {
+    const data = await AsyncStorage.getItem("liked");
+    console.log(data);
+  }
+
+  useEffect(() => {
+    getLikedSongs();
+  }, []);
+
   return (
     <View>
       <Text>favourite</Text>
     </View>
-  )
+  );
 }
--- src/components/common/KaraokeTile.tsx
+++ src/components/common/KaraokeTile.tsx
@@ -1,7 +1,8 @@
-import { View, Text } from "react-native";
 import React from "react";
-import { Avatar, ListItem } from "@rneui/base";
+import { Avatar, Icon, ListItem } from "@rneui/base";
 import { router } from "expo-router";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import kidExistsOnStorage from "../../utils/kidExistsOnStorage";
 
 interface Props {
   kid: number;
@@ -9,6 +10,38 @@ interface Props {
 }
 
 export default function KaraokeTile({ kid, title }: Props) {
+  const [liked, setLiked] = React.useState(false);
+
+  async function handleLike() {
+    if ((await AsyncStorage.getItem("liked")) === null) {
+      await AsyncStorage.setItem("liked", JSON.stringify([]));
+    }
+    const kidExists = await kidExistsOnStorage(kid);
+    const likedSongs = await AsyncStorage.getItem("liked");
+    console.log(kidExists, "kidExists");
+    console.log(likedSongs);
+
+    setLiked((prev) => !prev);
+    try {
+      const data = {
+        kid: kid,
+        title: title,
+      };
+
+      if (await kidExistsOnStorage(kid)) {
+        setLiked(true);
+      } else {
+        const likedSongs = await AsyncStorage.getItem("liked");
+        if (likedSongs) {
+          const parsedData = JSON.parse(likedSongs);
+          parsedData.push(data);
+          await AsyncStorage.setItem("liked", JSON.stringify(parsedData));
+          return;
+        }
+      }
+    } catch (error) {}
+  }
+
   return (
     <>
       <ListItem
@@ -23,7 +56,13 @@ export default function KaraokeTile({ kid, title }: Props) {
         <ListItem.Content>
           <ListItem.Title>{title}</ListItem.Title>
         </ListItem.Content>
-        <ListItem.Chevron size={25} color={"black"} />
+        {/* <ListItem.Chevron size={25} color={"black"} /> */}
+        <Icon
+          name={"heart"}
+          onPress={handleLike}
+          type={liked ? "font-awesome" : "feather"}
+          color={liked ? "red" : "black"}
+        />
       </ListItem>
     </>
   );
