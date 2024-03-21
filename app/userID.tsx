fd26b7aa72e4e8e054f7bea02ae1ef3e1da0503f
jatinhemnani01 <jatinhemnani01@gmail.com>
2024-03-21 18:05:21 +0530

    userID screen

--- app/userID.tsx
+++ app/userID.tsx
@@ -1,10 +1,29 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { ToastAndroid, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import Purchases from "react-native-purchases";
+import { Button, Text } from "@rneui/base";
+import tw from "twrnc";
 
 export default function UserID() {
+  const [userID, setUserID] = useState<string>("");
+
+  useEffect(() => {
+    async function userId() {
+      const id = await Purchases.getAppUserID();
+      setUserID(id);
+    }
+
+    userId();
+  }, []);
+
+  function copyToClipboard() {
+    ToastAndroid.show("Copied to clipboard", ToastAndroid.LONG);
+  }
+
   return (
-    <View>
-      <Text>userID</Text>
+    <View style={tw`p-2 flex justify-center items-center flex-1 gap-5`}>
+      <Text style={tw`font-bold text-md`}>{userID ?? ""}</Text>
+      <Button type="solid" title="Copy ID" onPress={copyToClipboard} />
     </View>
   );
 }
