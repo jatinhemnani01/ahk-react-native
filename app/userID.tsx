57d010258d833bb01ef8dedaafb21ff6a51fb976
jatinhemnani01 <jatinhemnani01@gmail.com>
2024-03-21 22:15:10 +0530

    added user id and copy to clipboard

--- app/userID.tsx
+++ app/userID.tsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 import Purchases from "react-native-purchases";
 import { Button, Text } from "@rneui/base";
 import tw from "twrnc";
+import * as Clipboard from "expo-clipboard";
 
 export default function UserID() {
   const [userID, setUserID] = useState<string>("");
@@ -16,13 +17,14 @@ export default function UserID() {
     userId();
   }, []);
 
-  function copyToClipboard() {
+  async function copyToClipboard() {
+    await Clipboard.setStringAsync(userID);
     ToastAndroid.show("Copied to clipboard", ToastAndroid.LONG);
   }
 
   return (
     <View style={tw`p-2 flex justify-center items-center flex-1 gap-5`}>
-      <Text style={tw`font-bold text-md`}>{userID ?? ""}</Text>
+      <Text style={tw`font-bold`}>{userID ?? ""}</Text>
       <Button type="solid" title="Copy ID" onPress={copyToClipboard} />
     </View>
   );
