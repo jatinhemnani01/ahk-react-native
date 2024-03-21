bd18e0bbe56c7c60dedc1e7914cfbf438854e6f8
jatinhemnani01 <jatinhemnani01@gmail.com>
2024-03-21 16:04:06 +0530

    feedback screen done

--- app/feedback.tsx
+++ app/feedback.tsx
@@ -1,10 +1,63 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {
+  Alert,
+  NativeSyntheticEvent,
+  TextInputChangeEventData,
+  View,
+} from "react-native";
+import React, { useState } from "react";
+import { Button, Input, Text } from "@rneui/base";
+import tw from "twrnc";
+import BASE_URL from "../src/constants/base_url";
 
 export default function Feedback() {
+  const [mobile, setMobile] = useState("");
+  const [message, setMessage] = useState("");
+  const [mobileError, setMobileError] = useState(true);
+
+  function changeMobile(e: NativeSyntheticEvent<TextInputChangeEventData>) {
+    setMobile(e.nativeEvent.text);
+    if (e.nativeEvent.text.length != 10) setMobileError(true);
+    else setMobileError(false);
+  }
+
+  function changeMessage(e: NativeSyntheticEvent<TextInputChangeEventData>) {
+    setMessage(e.nativeEvent.text);
+  }
+
+  function handleSubmit() {
+    fetch(`${BASE_URL.getState().baseURL}/feedback/add`, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify({
+        mobile,
+        message,
+      }),
+    }).finally(() => {
+      setMobile("");
+      setMessage("");
+      Alert.alert("Feedback Submitted Successfully");
+    });
+  }
+
   return (
-    <View>
-      <Text>feedback</Text>
+    <View style={tw`p-5`}>
+      <Input
+        keyboardType="phone-pad"
+        label="Mobile No."
+        onChange={changeMobile}
+        value={mobile}
+        errorMessage={mobileError ? "Invalid Mobile No." : ""}
+      />
+      <Input
+        keyboardType="name-phone-pad"
+        value={message}
+        label="Feedback Message"
+        onChange={changeMessage}
+      />
+
+      <Button onPress={handleSubmit} title={"Submit"} disabled={mobileError} />
     </View>
-  )
+  );
 }
