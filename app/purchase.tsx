b6cdaffe77f4983e861a92b8e87a9e6aaed47659
jatinhemnani01 <jatinhemnani01@gmail.com>
2024-03-22 23:12:37 +0530

    purchase screen UI

--- app/purchase.tsx
+++ app/purchase.tsx
@@ -1,10 +1,22 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View } from "react-native";
+import { Button, Text } from "@rneui/base";
+import React from "react";
+import tw from "twrnc";
+import { router } from "expo-router";
 
 export default function Purchase() {
   return (
-    <View>
-      <Text>purchase</Text>
+    <View style={tw`flex flex-1 justify-center items-center`}>
+      <Text style={tw`text-sm font-bold`}>
+        This Karaoke is only available for premium users.
+      </Text>
+      <Button
+        title={"Buy Now"}
+        type="clear"
+        onPress={() => {
+          router.navigate("/profile");
+        }}
+      />
     </View>
-  )
+  );
 }
