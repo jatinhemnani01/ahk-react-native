6a25fa7bbc808c7462d69656b66219a68f775c28
jatinhemnani01 <jatinhemnani01@gmail.com>
2024-04-03 14:12:40 +0530

    removed unused state

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,11 @@
 import { Pressable, View, Text } from "react-native";
 import tw from "twrnc";
 import { router } from "expo-router";
-import isProStore from "../../src/state/isPro";
 import KaraokeList from "../../src/components/home/KaraokeList";
 import BASE_URL from "../../src/constants/base_url";
 import BannerAdComp from "../../src/ads/BannerAdComp";
 
 export default function Home() {
-  const isPro = isProStore((state) => state.isPro);
   const baseURL = BASE_URL((state) => state.baseURL);
 
   return (
