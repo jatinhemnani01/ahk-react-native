0062001f0897613c9bc72a8faa994b7e4e6aeb01
jatinhemnani01 <jatinhemnani01@gmail.com>
2024-04-05 16:53:19 +0530

    notifications working

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -12,6 +12,9 @@ import BASE_URL from "../src/constants/base_url";
 import imgUrlState from "../src/state/imgUrlState";
 import { screens } from "../src/constants/screens";
 import { setStatusBarStyle } from "expo-status-bar";
+import messaging from "@react-native-firebase/messaging";
+import inAppMessaging from "@react-native-firebase/in-app-messaging";
+import { PermissionsAndroid } from "react-native";
 
 export default function RootLayout() {
   const remoteConfigService = new RemoteConfigService();
@@ -31,16 +34,50 @@ export default function RootLayout() {
     imgUrlState.setState({ url: imgUrl });
   }
 
+  // Request Notification Permission
+  async function requestUserPermission() {
+    PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS
+    );
+
+    const authStatus = await messaging().requestPermission();
+    const enabled =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+    if (enabled) {
+      console.log("Authorization status:", authStatus);
+    }
+  }
+
+  // In App Messaging
+  async function firebaseInAppMessage() {
+    messaging().requestPermission()
+  .then(() => messaging().getToken())
+  .then(token => {
+    console.log('FCM Installation ID:', token);
+  });
+    await inAppMessaging().setMessagesDisplaySuppressed(false);
+  }
+
   useEffect(() => {
     const timeout = setTimeout(() => {
       setStatusBarStyle("light");
     }, 3000);
 
-    fetchBaseURL();
+    // FETCHING BASE URL
+    // fetchBaseURL();
+
+    // IN APP MESSAGING
+    firebaseInAppMessage();
+
+    // calling requestUserPermission
+    requestUserPermission();
+
     async function setupPurchases() {
       Purchases.setLogLevel(LOG_LEVEL.DEBUG);
       if (Platform.OS === "android") {
-        Purchases.configure({ apiKey: "goog_gGxArHKrmaNHKqOSVLLiPWnlwYL", });
+        Purchases.configure({ apiKey: "goog_gGxArHKrmaNHKqOSVLLiPWnlwYL" });
       }
     }
     setupPurchases();
