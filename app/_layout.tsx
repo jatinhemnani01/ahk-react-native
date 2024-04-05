464dfc8dcc4fe83724a81646b81d29b890263bdf
jatinhemnani01 <jatinhemnani01@gmail.com>
2024-04-05 17:43:05 +0530

    notifications working

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,16 +5,15 @@ import Purchases, { LOG_LEVEL } from "react-native-purchases";
 import { Platform } from "react-native";
 import { updateSubscription } from "../src/subscription/getSubscription";
 import isProStore from "../src/state/isPro";
-import { getAnalytics } from "@react-native-firebase/analytics";
+import { firebase, getAnalytics } from "@react-native-firebase/analytics";
 import forAllState from "../src/state/forAllState";
 import { RemoteConfigService } from "../src/firebase/remoteConfig";
 import BASE_URL from "../src/constants/base_url";
 import imgUrlState from "../src/state/imgUrlState";
 import { screens } from "../src/constants/screens";
 import { setStatusBarStyle } from "expo-status-bar";
-import messaging from "@react-native-firebase/messaging";
 import inAppMessaging from "@react-native-firebase/in-app-messaging";
-import { PermissionsAndroid } from "react-native";
+import NotificationController from "../src/components/common/NotificationController";
 
 export default function RootLayout() {
   const remoteConfigService = new RemoteConfigService();
@@ -34,29 +33,8 @@ export default function RootLayout() {
     imgUrlState.setState({ url: imgUrl });
   }
 
-  // Request Notification Permission
-  async function requestUserPermission() {
-    PermissionsAndroid.request(
-      PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS
-    );
-
-    const authStatus = await messaging().requestPermission();
-    const enabled =
-      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
-      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
-
-    if (enabled) {
-      console.log("Authorization status:", authStatus);
-    }
-  }
-
   // In App Messaging
   async function firebaseInAppMessage() {
-    messaging().requestPermission()
-  .then(() => messaging().getToken())
-  .then(token => {
-    console.log('FCM Installation ID:', token);
-  });
     await inAppMessaging().setMessagesDisplaySuppressed(false);
   }
 
@@ -70,9 +48,6 @@ export default function RootLayout() {
 
     // IN APP MESSAGING
     firebaseInAppMessage();
-
-    // calling requestUserPermission
-    requestUserPermission();
 
     async function setupPurchases() {
       Purchases.setLogLevel(LOG_LEVEL.DEBUG);
@@ -99,6 +74,7 @@ export default function RootLayout() {
 
   return (
     <>
+      <NotificationController />
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
 
---
+++ src/components/common/NotificationController.tsx
@@ -0,0 +1,51 @@
+import React, { useEffect } from "react";
+import { Alert, PermissionsAndroid } from "react-native";
+import messaging from "@react-native-firebase/messaging";
+
+function NotificationController() {
+  async function requestUserPermission() {
+    PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS
+    );
+
+    const authStatus = await messaging().requestPermission();
+    const enabled =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+    if (enabled) {
+      console.log("Authorization status:", authStatus);
+    }
+  }
+
+  useEffect(() => {
+    requestUserPermission();
+
+    messaging()
+      .getInitialNotification()
+      .then((remoteMessage) => {
+        console.log("remoteMessage", remoteMessage);
+      });
+
+    messaging().onNotificationOpenedApp((remoteMessage) => {
+      console.log(
+        "Notification caused app to open from background state:",
+        remoteMessage
+      );
+    });
+
+    messaging().setBackgroundMessageHandler(async (remoteMessage) => {
+      console.log("Message handled in the background!", remoteMessage);
+    });
+
+    const unsubscribe = messaging().onMessage(async (remoteMessage) => {
+      Alert.alert("A new FCM message arrived!", JSON.stringify(remoteMessage));
+    });
+
+    return unsubscribe;
+  }, []);
+
+  return null;
+}
+
+export default NotificationController;
