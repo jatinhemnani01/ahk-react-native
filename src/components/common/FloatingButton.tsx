58fc797ec3ddc8fea0766d41fbb5c62725b406c7
jatinhemnani01 <jatinhemnani01@gmail.com>
2024-04-07 23:30:21 +0530

    floatin button actions

--- src/components/common/FloatingButton.tsx
+++ src/components/common/FloatingButton.tsx
@@ -1,26 +1,63 @@
 import React from "react";
 import { SpeedDial } from "@rneui/themed";
+import { colors } from "../../constants/colors";
+import { Linking } from "react-native";
+import { Link, router } from "expo-router";
 
 export default () => {
   const [open, setOpen] = React.useState(false);
+
+  function openURL(url: string) {
+    Linking.openURL(url);
+  }
+
+  function changeScreen(name: string) {
+    router.navigate(name);
+  }
+
   return (
     <SpeedDial
       style={{ zIndex: 1000 }}
+      color={colors.primary}
       isOpen={open}
-      icon={{ name: "edit", color: "#fff" }}
+      icon={{ name: "activity", color: "white", type: "feather" }}
       openIcon={{ name: "close", color: "#fff" }}
       onOpen={() => setOpen(!open)}
       onClose={() => setOpen(!open)}
     >
       <SpeedDial.Action
-        icon={{ name: "add", color: "#fff" }}
-        title="Add"
-        onPress={() => console.log("Add Something")}
+        icon={{ name: "star", color: "#fff", type: "feather" }}
+        color={colors.primary}
+        title="Rate Us"
+        onPress={() =>
+          openURL(
+            "https://play.google.com/store/apps/details?id=com.ahkkaraoke.app"
+          )
+        }
       />
       <SpeedDial.Action
-        icon={{ name: "delete", color: "#fff" }}
-        title="Delete"
-        onPress={() => console.log("Delete Something")}
+        icon={{ name: "bell", color: "#fff", type: "feather" }}
+        color={colors.primary}
+        title="Enable Notifications"
+        onPress={() => Linking.openSettings()}
+      />
+      <SpeedDial.Action
+        icon={{ name: "settings", color: "#fff", type: "feather" }}
+        title="Settings"
+        color={colors.primary}
+        onPress={() => changeScreen("/settings")}
+      />
+      <SpeedDial.Action
+        icon={{ name: "logo-whatsapp", color: "#fff", type: "ionicon" }}
+        color={colors.primary}
+        title="Contact Us"
+        onPress={() => openURL("whatsapp://send?phone=+918962210828")}
+      />
+      <SpeedDial.Action
+        icon={{ name: "hash", color: "#fff", type: "feather" }}
+        color={colors.primary}
+        title="User ID"
+        onPress={() => changeScreen("/userID")}
       />
     </SpeedDial>
   );
