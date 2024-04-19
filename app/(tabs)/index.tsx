846832a5eaef3b41acc99e36c2eb229edd57550a
jatinhemnani01 <jatinhemnani01@gmail.com>
2024-04-19 18:03:06 +0530

    removed unused imports

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,4 @@
-import { Pressable, View, Text } from "react-native";
-import tw from "twrnc";
-import { router } from "expo-router";
-import KaraokeList from "../../src/components/home/KaraokeList";
 import BASE_URL from "../../src/constants/base_url";
-import BannerAdComp from "../../src/ads/BannerAdComp";
 import inAppMessaging from "@react-native-firebase/in-app-messaging";
 import { useEffect } from "react";
 import analytics from "@react-native-firebase/analytics";
@@ -26,19 +21,6 @@ export default function Home() {
 
   return (
     <>
-      {/* <View style={{ height: "100%" }}>
-        <BannerAdComp />
-        <Pressable
-          onPress={() => router.navigate("/popular")}
-          style={tw`flex flex-row justify-center m-2`}
-        >
-          <Text style={tw`text-lg text-blue-500 font-semibold`}>
-            Click Here To See Popular Karaoke
-          </Text>
-        </Pressable>
-
-        {baseURL === "" ? null : <KaraokeList />}
-      </View> */}
       <HomeTabs />
     </>
   );
