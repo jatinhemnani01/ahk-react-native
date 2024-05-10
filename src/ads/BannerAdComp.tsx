8613460f86aca3704c4563f1de613c1d4509d740
jatinhemnani01 <jatinhemnani01@gmail.com>
2024-05-10 17:52:02 +0530

    ios banenr ad id

--- src/ads/BannerAdComp.tsx
+++ src/ads/BannerAdComp.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from "react-native";
+import { View, Text, Platform } from "react-native";
 import { BannerAd, BannerAdSize } from "react-native-google-mobile-ads";
 import React from "react";
 import forAllState from "../state/forAllState";
@@ -7,16 +7,16 @@ import isProStore from "../state/isPro";
 export default function BannerAdComp() {
   const isPro = isProStore((state) => state.isPro);
   const forAll = forAllState((state) => state.forAll);
+  const android = "ca-app-pub-5153786927158690/1601135861";
+  const ios = "ca-app-pub-5153786927158690/4936385809";
+  const adId = Platform.OS === "android" ? android : ios;
 
   const Ad = () => {
     if (isPro) return null;
     if (!forAll) return null;
 
     return (
-      <BannerAd
-        size={BannerAdSize.ANCHORED_ADAPTIVE_BANNER}
-        unitId={"ca-app-pub-5153786927158690/1601135861"}
-      />
+      <BannerAd size={BannerAdSize.ANCHORED_ADAPTIVE_BANNER} unitId={adId} />
     );
   };
 
