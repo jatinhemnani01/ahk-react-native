98814d2e64e36ca75dfe786909d678e23a8aed71
jatinhemnani01 <jatinhemnani01@gmail.com>
2024-05-16 14:26:41 +0530

    added alert for ios

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,4 +1,4 @@
-import { Alert, View } from "react-native";
+import { Alert, Platform, View } from "react-native";
 import RevenueCatUI, { PAYWALL_RESULT } from "react-native-purchases-ui";
 import tw from "twrnc";
 import { updateSubscription } from "../../src/subscription/getSubscription";
@@ -12,8 +12,16 @@ export default function profile() {
   const isPro = isProStore((state) => state.isPro);
   const toPro = isProStore((state) => state.toPro);
   const toFree = isProStore((state) => state.toFree);
+  const isIos = Platform.OS === "ios";
 
   async function displayPaywall() {
+    if (isIos) {
+      Alert.alert(
+        "Subscription",
+        "• Payment will be charged to iTunes Account at confirmation of purchase \n • Subscription automatically renews unless auto-renew is turned off at least 24-hours before the end of the current period. \n Cancel anytime Settings > Apple ID one day before renewal date"
+      );
+    }
+
     const paywallResult: PAYWALL_RESULT = await RevenueCatUI.presentPaywall();
     if (paywallResult === PAYWALL_RESULT.PURCHASED) {
       toPro();
